0f61f1ddeb004ebd6f442be69c750d42cc152259
clara004 <116096892+clara004@users.noreply.github.com>
2023-05-17 16:46:00 +0100

    Update Color.cpp
    comments

--- Color.cpp
+++ Color.cpp
@@ -6,33 +6,33 @@ namespace prog {
         green_=0;
         blue_=0;
     }
-    Color::Color(const Color& other) {
+    Color::Color(const Color& other) { //constructor copy
         red_=other.red();
         green_=other.green();
         blue_=other.blue();
     }
-    Color::Color(rgb_value red, rgb_value green, rgb_value blue) {
+    Color::Color(rgb_value red, rgb_value green, rgb_value blue) { // construtor com parâmetros
         red_=red;
         green_=green;
         blue_=blue;
     }
-    rgb_value Color::red() const {
+    rgb_value Color::red() const {  //getter para a componente da cor vermelha
         return red_;
     }
-    rgb_value Color::green() const {
+    rgb_value Color::green() const { //getter para a componente da cor verde
         return green_;
     }
-    rgb_value Color::blue() const {
+    rgb_value Color::blue() const { //getter para a componente da cor azul
         return blue_;
     }
 
-    rgb_value& Color::red()  {
+    rgb_value& Color::red()  {  //getter e setter para a componente da cor vermelha
         return red_;
     }
-    rgb_value& Color::green()  {
+    rgb_value& Color::green()  {    //getter e setter para a componente da cor verde
       return green_;
     }
-    rgb_value& Color::blue()  {
+    rgb_value& Color::blue()  { //getter e setter para a componente da cor azul
       return blue_;
     }
 }
