ffb1f02021993623b2720ea05e725a21de285b18
clara004 <116096892+clara004@users.noreply.github.com>
2023-05-17 17:09:06 +0100

    Update Image.hpp
    comments

--- Image.hpp
+++ Image.hpp
@@ -9,24 +9,24 @@ namespace prog
   class Image
   {
   private:
-    int w_;
-    int h_;
-    Color color_;
-    std::vector<std::vector<Color>> array;
+    int w_;  // largura da imagem
+    int h_;  // altura da imagem
+    Color color_; // cor padrão
+    std::vector<std::vector<Color>> array; // matriz de pixels
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255});
-    ~Image();
+    Image(int w, int h, const Color &fill = {255, 255, 255}); // constructor da imagem
+    ~Image(); // destructor da imagem
     int width() const;
     int height() const;
-    Color &at(int x, int y);
+    Color &at(int x, int y); // obter uma referência para o pixel na posição (x, y)
     const Color &at(int x, int y) const;
-    void invert();
-    void to_gray_scale();
-    void replace(rgb_value red1,rgb_value green1,rgb_value blue1,rgb_value red2,rgb_value green2,rgb_value blue2);
-    void fill(int x,int y,int w,int h,rgb_value red,rgb_value green,rgb_value blue);
-    void h_mirror();
-    void l_mirror();
-    void add_filename(rgb_value r,rgb_value g,rgb_value b,int x,int y);
+    void invert(); // inverter as cores da imagem
+    void to_gray_scale(); // converter a imagem para escala de cinzento
+    void replace(rgb_value red1,rgb_value green1,rgb_value blue1,rgb_value red2,rgb_value green2,rgb_value blue2);  // substituir uma cor por outra
+    void fill(int x,int y,int w,int h,rgb_value red,rgb_value green,rgb_value blue); // preencher uma região retangular com uma cor
+    void h_mirror(); // espelhar a imagem horizontalmente
+    void l_mirror(); // espelhar a imagem verticalmente
+    void add_filename(rgb_value r,rgb_value g,rgb_value b,int x,int y); // adicionar um filename na posição (x, y)
   };
 }
 #endif
