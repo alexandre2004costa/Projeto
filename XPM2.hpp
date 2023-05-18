293ffe6feea9576776168224be63d34999076ad4
clara004 <116096892+clara004@users.noreply.github.com>
2023-05-18 16:00:34 +0100

    Update XPM2.hpp
    comentario

--- XPM2.hpp
+++ XPM2.hpp
@@ -5,8 +5,10 @@
 #include "Image.hpp"
 
 namespace prog {
+    // Função para carregar uma imagem no formato XPM2 a partir de um arquivo
     Image* loadFromXPM2(const std::string &file);
-
+    
+    // Função para salvar uma imagem no formato XPM2 em um arquivo
     void saveToXPM2(const std::string& file, const Image* img);
 }
 #endif
