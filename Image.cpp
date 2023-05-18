0b45c24f78817cd3c835640b21b6aa3ac9620ccf
clara004 <116096892+clara004@users.noreply.github.com>
2023-05-18 15:29:31 +0100

    Update Image.cpp
    comments

--- Image.cpp
+++ Image.cpp
@@ -3,31 +3,31 @@
 namespace prog
 {
   Image::Image(int w, int h, const Color &fill){
-    w_=w;
-    h_=h;
-    array.resize(w_, std::vector<Color>(h_));
+    w_=w; // largura da imagem
+    h_=h; // altura da imagem
+    array.resize(w_, std::vector<Color>(h_)); // redimensionar a matriz de pixels
     for(int i=0;i<w_;i++){
       for(int j=0;j<h_;j++){
-        array[i][j]=fill;
+        array[i][j]=fill; // preencher a matriz de pixels com a cor especificada
       }
     }
   }
   Image::~Image(){}
-  int Image::width() const
+  int Image::width() const 
   {
-    return w_;
+    return w_; // obter a largura da imagem
   }
   int Image::height() const
   {
-    return h_;
+    return h_; // obter a altura da imagem
   }
   Color& Image::at(int x, int y)
   {
-    return array[x][y];
+    return array[x][y]; // obter uma referência para o pixel na posição (x, y)
   }
   const Color& Image::at(int x, int y) const
   {
-    return array[x][y];
+    return array[x][y]; // obter uma referência constante para o pixel na posição (x, y)
   }
   
 }
