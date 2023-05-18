5c0c8086dd0ce7686ec21aefecfd450ae56244e0
clara004 <116096892+clara004@users.noreply.github.com>
2023-05-18 15:31:48 +0100

    Update Image.hpp
    comentarios

--- Image.hpp
+++ Image.hpp
@@ -9,16 +9,16 @@ namespace prog
   class Image
   {
   private:
-    int w_;
-    int h_;
-    std::vector<std::vector<Color>> array;
+    int w_; // largura da imagem
+    int h_; // altura da imagem
+    std::vector<std::vector<Color>> array; //matriz dos pixels
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255});
-    ~Image();
-    int width() const;
-    int height() const;
-    Color &at(int x, int y);
-    const Color &at(int x, int y) const;
+    Image(int w, int h, const Color &fill = {255, 255, 255}); // constructor da imagem
+    ~Image(); // distructor da imagem
+    int width() const; //obter a largura da imagem
+    int height() const; //obter a altura da imagem
+    Color &at(int x, int y); // obter uma referência para o pixel na posição (x, y)
+    const Color &at(int x, int y) const; // obter uma referência constante para o pixel na posição (x, y)
   };
 }
 #endif
