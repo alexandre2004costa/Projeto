830016fa26abd4ef821c85eef3155b2d872e09a9
alexandre2004costa <108695812+alexandre2004costa@users.noreply.github.com>
2023-05-18 15:12:41 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -9,24 +9,16 @@ namespace prog
   class Image
   {
   private:
-    int w_;  // largura da imagem
-    int h_;  // altura da imagem
-    Color color_; // cor padrão
-    std::vector<std::vector<Color>> array; // matriz de pixels
+    int w_;
+    int h_;
+    std::vector<std::vector<Color>> array;
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255}); // constructor da imagem
-    ~Image(); // destructor da imagem
+    Image(int w, int h, const Color &fill = {255, 255, 255});
+    ~Image();
     int width() const;
     int height() const;
-    Color &at(int x, int y); // obter uma referência para o pixel na posição (x, y)
+    Color &at(int x, int y);
     const Color &at(int x, int y) const;
-    void invert(); // inverter as cores da imagem
-    void to_gray_scale(); // converter a imagem para escala de cinzento
-    void replace(rgb_value red1,rgb_value green1,rgb_value blue1,rgb_value red2,rgb_value green2,rgb_value blue2);  // substituir uma cor por outra
-    void fill(int x,int y,int w,int h,rgb_value red,rgb_value green,rgb_value blue); // preencher uma região retangular com uma cor
-    void h_mirror(); // espelhar a imagem horizontalmente
-    void l_mirror(); // espelhar a imagem verticalmente
-    void add_filename(rgb_value r,rgb_value g,rgb_value b,int x,int y); // adicionar um filename na posição (x, y)
   };
 }
 #endif
